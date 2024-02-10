f169538fefbf676c72d8cc03cd2508814a0137ac
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-02-10 09:04:51 -0500

    Finished Review of Lesson 6 Conditionals

--- main.js
+++ main.js
@@ -280,3 +280,54 @@ for(var counter = 0; counter < moviesArray.length; counter++){
 /***************************************************************************************************************************************/
 /***************************************************************************************************************************************/
 
+
+
+                                               /* Fundamentals 6 - Conditionals */
+
+//Conditionals decide whether or not pieces of code should run:
+
+if(2+2 === 4){
+    console.log("Math works!");  //<---- Expected output
+} else {
+    console.log("Math is broken.")
+}
+
+/* You can add as many conditions as you want with 'else if'. JavaScript will try each condition in order. As soon as on of them
+evaluates to 'true', it will run that code block and skip the rest. */
+
+// Triple equals will check to see if the type and the value are equal
+"Hello" === "Hello"; //true
+24 === "24"; // false because an integer does not equal a string value.
+
+// Double equals will compare values as if they're the same type, even if they're not. This can get messy, so you should default to triple equals 
+24 == "24" // true
+
+// To check to see if things are NOT equal:
+var name = "Jordan";
+if(name !== "Dwayne"){
+  console.log("Too bad for you!");  // <-- Expected output
+}
+
+        /* Logical Operators - checking to see if more than one condition is true */
+
+var fullMoon = true;
+var isWolf = false;
+
+// This block of code will only run if the moon is full **--AND--** you're a wolf. Right now, this code won't run because the value of isWolf is false.
+
+if(fullMoon && isWolf){  // '&&' = AND (both conditions must be true)
+    console.log("AWOOOOOOOOOOO");
+}
+
+/*What if we want non-wolves to be able to howl at the full moon too? This block will run if you're a wolf **--OR--** if the moon is full. 
+However, if both conditions are false (you're not a wolf and the moon is not full), the code will not run.
+*/
+if(fullMoon || isWolf){  // '||' = OR (at least one condition must be true)
+    console.log("AWOOOOOOOOOOO");
+}
+
+
+/***************************************************************************************************************************************/
+/***************************************************************************************************************************************/
+
+
